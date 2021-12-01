3f1e3334d5f7e73e170909bf72b7ebac5bb5215c
sangmok <use0504@naver.com>
2021-12-01 19:11:43 +0900

    no message

--- ops.py
+++ ops.py
@@ -1,6 +1,8 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
 
 df = pd.read_csv('als.CSV', index_col=0, encoding='CP949')
 df.head()
@@ -12,8 +14,9 @@ plt.title('야구')
 plt.xlabel('ops')
 plt.ylabel('타점')
 
-line_fitter = LinearRegression()
-line_fitter.fit(X.values.reshape(-1,1), y)
+X_train, X_test, y_train, y_test = train_test_split(X.values.reshape(-1,1), y, train_size=0.8, test_size=0.2)
+
+line_fitter = LinearRegression().fit(X_train, y_train)
 
 plt.plot(X, y, 'o')
 plt.plot(X,line_fitter.predict(X.values.reshape(-1,1)))
@@ -24,3 +27,5 @@ print('입력한 ops에 대한 타점 예측값 :',round(a,3))
 k = int(input('시각화?'))
 if k == 1:
   plt.show()
+
+print(line_fitter.score(X_train, y_train))
